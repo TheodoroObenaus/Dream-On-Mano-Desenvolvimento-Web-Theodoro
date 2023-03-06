4fe752bd918656c814de4b9242b1474eb72632dd
TheodoroObenaus <theodoro.obenaus@gmail.com>
2023-03-06 11:31:37 -0300

    gigaton brabo

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,48 @@
-<script setup>
-import { ref, onMounted } from 'vue'
+  <script setup>
+  import { ref, onMounted } from 'vue'
 
-const contador = ref(0)
+  const contador = ref(0)
 
-function incrementarContador() {
-  this.contador.value++
-}
-function decrementarContador() {
-  if (this.contador > 0) {
-  this.contador.value--
+  function incrementarContador() {
+  contador.value++
   }
-}
+  function decrementarContador() {
+    if (contador.value > 0) {
+    contador.value--
+    }
+  }
 
-onMounted(() => {
-  console.log(`O valor inicial do contador é ${contador.value}.`)
-})
-</script>
+  function resetar() {
 
-<template>
-  <button @click="incrementarContador">Mais</button>
-  <button @click="decrementarContador">Menos</button>
-  <p>Valor do contador é: {{ contador }}</p>
-</template>
+  contador.value = 0
+  }
 
-<style scoped>
-p {
-  border: 3px solid gray;
-  border-radius: 10px;
-  background-color: black;
-  color: white;
-}
-button {
-  font-weight: bold;
-  background-color: black;
-  border: 3px solid gray;
-  border-radius: 10px;
-  color: white;
-} 
-</style>
+  onMounted(() => {
+    console.log(`O valor inicial do contador é ${contador.value}.`)
+  })
+  </script>
+
+  <template>
+    <button @click="incrementarContador">Mais</button>
+    <button @click="decrementarContador">Menos</button>
+    <button @click="resetar">reset</button>
+    <p>Valor do contador é: {{ contador }}</p>
+  </template>
+
+  <style scoped>
+  p {
+    border: 3px solid gray;
+    border-radius: 10px;
+    background-color: black;
+    color: white;
+    margin: 10px 10px 10px 10px;
+  }
+  button {
+    font-weight: bold;
+    background-color: black;
+    border: 3px solid gray;
+    border-radius: 10px;
+    color: white;
+    margin: 10px 10px 10px 10px;
+  } 
+  </style>
